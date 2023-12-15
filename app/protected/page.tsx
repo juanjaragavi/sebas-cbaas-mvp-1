262543e00b6f55cf77d0d715b0690779ab71d1ef
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 15:17:42 -0500

    Refactor onboarding component and iframe
    This commit refactors the onboarding component and iframe in the protected page. The changes include removing the unused PopupButton component and updating the classNames and attributes of the iframe.

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,28 +1,16 @@
 import SignOut from "@/components/sign-out";
-import { PopupButton } from '@typeform/embed-react'
-
-const Onboarding = () => {
-  return (
-    <PopupButton id="<form-id>" style={{ fontSize: 20 }} className="my-button">
-      click to open form in popup
-    </PopupButton>
-  )
-}
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-white">
-      <div className="w-full h-full flex flex-col space-y-5 justify-center items-center">
-        <Onboarding/>
-        <iframe
-          src="https://juanjaramilloai.typeform.com/to/LROMk6Zb"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          className="w-full h-full"
-        ></iframe>
-        <SignOut />
-      </div>
+    <div className="min-w-full min-h-full bg-white">
+      <iframe
+        src="https://juanjaramilloai.typeform.com/to/LROMk6Zb"
+        title="Typeform Onboarding Form"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="min-w-full min-h-full"
+      ></iframe>
+      <SignOut />
     </div>
   );
 }
