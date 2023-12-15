f216e2f5ff6458e4338b4d11b9c3bbc5c4be86e0
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 14:21:08 -0500

    Update Widget component in protected page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,11 +1,25 @@
 import SignOut from "@/components/sign-out";
 import { Widget } from '@typeform/embed-react'
 
+
 export default function Home() {
+  const widgetContainerStyle = {
+    width: 500,
+    height: 400,
+    margin: '20px auto',
+  }
+
   return (
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
-        <Widget id="LROMk6Zb" style={{ width: '50%' }} />
+        <Widget
+          id="LROMk6Zb"
+          style={widgetContainerStyle}
+          medium="demo-test"
+          hidden={{ foo: 'foo value', bar: 'bar value' }}
+          transitiveSearchParams={['foo', 'bar']}
+          iframeProps={{ title: 'Foo Bar' }}
+        />
         <iframe
           src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
           title="YouTube video player"
