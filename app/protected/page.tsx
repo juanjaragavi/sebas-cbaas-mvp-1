75d1ae1cefb09e9634afb0e82d8cb7fe3fe842da
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 12:43:49 -0500

    Typeform Imported 4

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
-        <Widget id="LROMk6Zb" style={{ width: '50%' }} />
+        <iframe
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="w-full max-w-screen-lg aspect-video"
+        ></iframe>
         <SignOut />
       </div>
     </div>
