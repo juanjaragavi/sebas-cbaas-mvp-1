3e8e92f643abdc75d520edd986bad954f3f4fb05
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 12:42:46 -0500

    Typeform Imported 3

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex h-screen bg-black">
       <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center">
-      <Widget id="LROMk6Zb" style={{ width: '50%' }} className="my-form" />
+        <Widget id="LROMk6Zb" style={{ width: '50%' }} />
         <SignOut />
       </div>
     </div>
