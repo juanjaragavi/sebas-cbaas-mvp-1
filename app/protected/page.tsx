08bb04c8af9e9cde3054cf2db0feccb06b114eab
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 15:30:15 -0500

    Update page layout and title

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,13 +2,13 @@ import SignOut from "@/components/sign-out";
 
 export default function Home() {
   return (
-    <div className="min-w-full h-full min-h-full bg-white">
+    <div className="min-w-full h-screen flex flex-col justify-center items-center bg-white">
       <iframe
         src="https://juanjaramilloai.typeform.com/to/LROMk6Zb"
-        title="Typeform Onboarding Form"
+        title="¡Hola, Soy Sebas y te voy a ayudar a configurar tu Bot!"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
-        className="min-w-full h-full min-h-full"
+        className="min-w-full h-screen"
       ></iframe>
       <SignOut />
     </div>
