8d885103acff7f73dd4211116d5404da50237be8
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 16:16:56 -0500

    Add typeform popover to onboarding page

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,8 +1,15 @@
 import SignOut from "@/components/sign-out";
+import { Popover } from '@typeform/embed-react'
+
+const formId = 'crNvgGG2'
 
 export default function Home() {
   return (
     <div className="min-w-full h-screen flex flex-col justify-center items-center bg-white">
+      <Popover
+          id={formId}
+          autoClose
+        />
       <iframe
         src="https://juanjaramilloai.typeform.com/to/LROMk6Zb"
         title="¡Hola, Soy Sebas y te voy a ayudar a configurar tu Bot!"
