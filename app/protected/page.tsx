e54e80b989da4b6cfd355e818d5b882996f734cc
juanjaragavi <info@juanjaramillo.tech>
2023-12-15 15:10:23 -0500

    Refactor page.tsx to use Onboarding component and add YouTube video player

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,15 +1,22 @@
-import { Widget } from "@typeform/embed-react";
 import SignOut from "@/components/sign-out";
+import { Widget } from '@typeform/embed-react'
+
+const Onboarding = () => {
+  return <Widget id="LROMk6Zb" style={{ width: '100%' }} />
+}
 
 export default function Home() {
   return (
-    <div className="flex w-full h-full bg-white">
+    <div className="flex h-screen bg-white">
       <div className="w-full h-full flex flex-col space-y-5 justify-center items-center">
-        <Widget
-          id="LROMk6Zb"
-          style={{ width: "500px", height: "400px" }}
-          className="my-form"
-        />
+        <Onboarding/>
+        <iframe
+          src="https://juanjaramilloai.typeform.com/to/LROMk6Zb"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          className="w-full h-full"
+        ></iframe>
         <SignOut />
       </div>
     </div>
